c889d06ab423e14dd96048fda5738c456458323f
Nikita Ruhtin <rns@d-element.ru>
2024-03-29 12:51:48 +0500

    add: new components, custom hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useEffect, useMemo, useRef, useState} from "react";
 import Counter from "./components/Counter";
 import "./styles/App.css";
 import PostItem from "./components/PostItem";
@@ -7,33 +7,49 @@ import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal"
+import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: "JavaScript 1", body: "Description"},
-        {id: 2, title: "JavaScript 2", body: "Description"},
-        {id: 3, title: "JavaScript 3", body: "Description"},
-    ])
+    const [posts, setPosts] = useState([])
+
+    const [filter, setFilter] = useState({sort: "", query: ""})
+    const [modal, setModal] = useState(false)
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
+        setModal(false)
     }
 
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
+    async function fetchPosts() {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        setPosts(response.data)
+    }
 
     return (
         <div className="App">
-            <PostForm create={createPost}/>
-            <MySelect
-                defaultValue="Сортировка"
-                options={[
-                    {value: "title", name: "По названию"},
-                    {value: "body", name: "По описанию"}
-                ]}
+            <MyButton style={{marginTop: "30px"}} onClick={() => setModal(true)}>
+                Создать пользователя
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
+            <hr style={{margin: "15px 0"}}/>
+            <PostFilter
+                filter={filter}
+                setFilter={setFilter}
             />
-            <PostList remove={removePost} posts={posts} title="Посты про JS"/>
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
         </div>
     );
 }
