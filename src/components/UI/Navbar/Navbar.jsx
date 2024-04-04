2769fcdec578c50d484465bf0f048aedd8c09b02
Nikita Ruhtin <rns@d-element.ru>
2024-04-04 16:00:46 +0500

    fix: styles

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -13,12 +13,14 @@ const Navbar = () => {
 
     return (
         <div className="navbar">
-            <MyButton onClick={logout}>
-                Выйти
-            </MyButton>
-            <div className="navbar__links">
-                <Link to="/about">О сайте</Link>
-                <Link to="/posts">Посты</Link>
+            <div className="navbar__container">
+                <MyButton onClick={logout}>
+                    Выйти
+                </MyButton>
+                <div className="navbar__links">
+                    <Link to="/about">О сайте</Link>
+                    <Link to="/posts">Посты</Link>
+                </div>
             </div>
         </div>
     );
